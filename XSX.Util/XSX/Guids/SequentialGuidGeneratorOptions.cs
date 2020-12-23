ba23bb51e13a60e59369678d9d0f6e3cf5e420a4
1747623132@qq.com <1747623132@qq.com>
2020-12-23 11:19:34 +0800

    GuidGenerator

---
+++ XSX.Util/XSX/Guids/SequentialGuidGeneratorOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XSX.Guids
+{
+    public class SequentialGuidGeneratorOptions
+    {
+        /// <summary>
+        /// Default value: <see cref="SequentialGuidType.SequentialAtEnd"/>.
+        /// </summary>
+        public SequentialGuidType DefaultSequentialGuidType { get; set; }
+
+        public SequentialGuidGeneratorOptions()
+        {
+            DefaultSequentialGuidType = SequentialGuidType.SequentialAtEnd;
+        }
+    }
+}
