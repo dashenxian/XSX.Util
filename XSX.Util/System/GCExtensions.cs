402cf83ed735f2c0365720e2e9ce363b997bc002
1747623132@qq.com <1747623132@qq.com>
2021-01-28 11:11:46 +0800

    添加GC压缩大对象方法

---
+++ XSX.Util/System/GCExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Text;
+
+namespace System
+{
+    public static class GCExtensions
+    {
+        public static void CollectLarge()
+        {
+            GCSettings.LargeObjectHeapCompactionMode =
+                GCLargeObjectHeapCompactionMode.CompactOnce;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
