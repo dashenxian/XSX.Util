7d15c9a07a959a845e530ff8a5f94ccb8fea4a71
1747623132@qq.com <1747623132@qq.com>
2021-03-02 18:02:02 +0800

    获取静态字段或属性的值

--- XSX.Util/XSX/Helper/ReflectionHelper.cs
+++ XSX.Util/XSX/Helper/ReflectionHelper.cs
@@ -37,5 +37,65 @@ namespace XSX.Util.XSX.Helper
 
             return IsAssignableToGenericType(givenTypeInfo.BaseType, genericType);
         }
+        /// <summary>
+        /// 获取静态属性或静态字段的值
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="name">字段或属性名称</param>
+        /// <param name="value">值</param>
+        /// <returns>是否获取成功</returns>
+        public static bool TryGetStaticFieldOrPropertyValue(Type type, string name, out object value)
+        {
+            Type temp = type;
+
+            do
+            {
+                var field = temp.GetField(name, BindingFlags.Public | BindingFlags.Static);
+                if (field != null)
+                {
+                    value = field.GetValue(null);
+                    return true;
+                }
+
+                temp = temp.BaseType;
+            } while (temp != null);
+
+
+            temp = type;
+
+            do
+            {
+                var prop = temp.GetProperty(name, BindingFlags.Public | BindingFlags.Static);
+                if (prop != null)
+                {
+                    value = prop.GetValue(null, null);
+                    return true;
+                }
+
+                temp = temp.BaseType;
+            } while (temp != null);
+
+            value = null;
+            return false;
+        }
+        /// <summary>
+        /// 获取静态属性或静态字段的值
+        /// </summary>
+        /// <typeparam name="T">字段或属性类型</typeparam>
+        /// <param name="type">类型</param>
+        /// <param name="name">字段或属性名称</param>
+        /// <param name="value">值</param>
+        /// <returns>是否获取成功</returns>
+        public static bool TryGetStaticFieldOrPropertyValue<T>(Type type, string name, out T value)
+        {
+            var result = TryGetStaticFieldOrPropertyValue(type, name, out object obj);
+            if (result && obj is T)
+            {
+                value = (T)obj;
+                return true;
+            }
+            value = default(T);
+            return false;
+        }
     }
 }
