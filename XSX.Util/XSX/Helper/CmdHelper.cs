75cee0cd97987835a96d0b6525b4e79862c16ae0
1747623132@qq.com <1747623132@qq.com>
2022-08-03 14:15:52 +0800

    添加执行CMD命令并接受列表类型结果

--- XSX.Util/XSX/Helper/CmdHelper.cs
+++ XSX.Util/XSX/Helper/CmdHelper.cs
@@ -98,5 +98,78 @@ namespace XSX.Helper
             //Windows default.
             return "cmd.exe";
         }
+        public static List<string> RunCmdAndGetOutput(string command, string arguments)
+        {
+            using var process = new Process
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            var errorBuilder = new StringBuilder();
+            var output = new List<string>();
+            process.OutputDataReceived += (_, args) =>
+            {
+                if (args.Data != null)
+                {
+                    output.Add(args.Data);
+                }
+            };
+            process.BeginOutputReadLine();
+            process.ErrorDataReceived += (_, args) =>
+            {
+                if (args.Data != null)
+                {
+                    errorBuilder.AppendLine(args.Data);
+                }
+            };
+            process.BeginErrorReadLine();
+            if (!process.DoubleWaitForExit())
+            {
+                var timeoutError = $@"Process timed out. Command line: {command} {arguments}.
+Output: {string.Join(Environment.NewLine, output)}
+Error: {errorBuilder}";
+                throw new Exception(timeoutError);
+            }
+
+            if (process.ExitCode == 0)
+            {
+                return output;
+            }
+
+
+            var errors = errorBuilder.ToString();
+            if (errors.Contains("Cannot find a tool in the manifest file that has a command named"))
+            {
+                throw new Exception("The dotnet tool `dotnet-symbol` was not found.");
+            }
+
+            var error = $@"Could not execute process. Command line: {command} {arguments}.
+Output: {string.Join(Environment.NewLine, output)}
+Error: {errors}";
+            throw new Exception(error);
+        }
+
+        public static bool DoubleWaitForExit(this Process process)
+        {
+            //4min30sec
+            var timeout = 270000;
+            var result = process.WaitForExit(timeout);
+            if (result)
+            {
+                process.WaitForExit();
+            }
+
+            return result;
+        }
     }
 }
