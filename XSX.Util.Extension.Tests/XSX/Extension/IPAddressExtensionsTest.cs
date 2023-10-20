5c98dac019924e94be867da8b9ce2a4a6f086c06
xiaoshenxian <125880321@qq.com>
2023-10-20 13:15:41 +0800

    添加判断ip地址是否在网段内方法

---
+++ XSX.Util.Extension.Tests/XSX/Extension/IPAddressExtensionsTest.cs
@@ -0,0 +1,35 @@
+﻿
+using Shouldly;
+using XSX.Extension;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests.XSX.Extension
+{
+    public class IPAddressExtensionsTest
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public IPAddressExtensionsTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+        [Fact]
+        public void IpAddressInRangeTest()
+        {
+            var ip = "192.168.2.3";
+            var start = "192.168.1.1";
+            var end = "192.168.2.255";
+            ip.IpAddressInRange(start, end).ShouldBe(true);
+        }
+        [Fact]
+        public void IpAddressOutRangeTest()
+        {
+            var ip = "192.168.3.3";
+            var start = "192.168.1.1";
+            var end = "192.168.2.255";
+            ip.IpAddressInRange(start, end).ShouldBe(false);
+        }
+
+    }
+}
--- XSX.Util/XSX/Extension/IPAddressExtensions.cs
+++ XSX.Util/XSX/Extension/IPAddressExtensions.cs
@@ -22,5 +22,37 @@ namespace XSX.Extension
             var port = ipEndPoint.Port;
             return port;
         }
+        /// <summary>
+        /// 判断IP地址在不在某个IP地址段
+        /// </summary>
+        /// <param name="input">需要判断的IP地址</param>
+        /// <param name="begin">起始地址</param>
+        /// <param name="ends">结束地址</param>
+        /// <returns></returns>
+        public static bool IpAddressInRange(this string input, string begin, string ends)
+        {
+            uint current = IPToID(input);
+            return current >= IPToID(begin) && current <= IPToID(ends);
+        }
+        /// <summary>
+        /// IP地址转换成数字
+        /// </summary>
+        /// <param name="addr">IP地址</param>
+        /// <returns>数字,输入无效IP地址返回0</returns>
+        private static uint IPToID(string addr)
+        {
+            if (!IPAddress.TryParse(addr, out var ip))
+            {
+                return 0;
+            }
+
+            byte[] bInt = ip.GetAddressBytes();
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bInt);
+            }
+
+            return BitConverter.ToUInt32(bInt, 0);
+        }
     }
 }
