cad6d5fd95887954e46621bf21b4b4e49ace0dd6
xiaoshenxian <125880321@qq.com>
2023-10-27 14:58:46 +0800

    增加四参数坐标转换

---
+++ XSX.Util.Extension.Tests/XSX/Util/PointTransformaTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+using XSX.Util;
+using Xunit;
+
+namespace Tests.XSX.Util
+{
+    public class PointTransformaTest
+    {
+        [Fact]
+        public void TransformaTest()
+        {
+            var standardPoints = new Dictionary<Point, Point>()
+            {
+                { new Point(653068.71271461656, 3281704.0923241354), new Point(651796.2895339021, 3281825.0074003497) },
+                { new Point(653177.54815978638, 3281675.9374143807), new Point(651903.916431491, 3281857.4774478413) },
+                { new Point(653068.7127, 3281704.0923), new Point(651796.2895, 3281825.0074) },
+                { new Point(653178.5482, 3281675.9410), new Point(651904.7691, 3281857.9999) }
+            };
+            
+            var transformaParam = new TransformationParam(standardPoints);
+            var pointTransforma = new PointTransforma(transformaParam);
+            var targetPoint = pointTransforma.Transforma(new Point(653122.0487, 3281691.3951));
+            Math.Abs(targetPoint.X - 651848.4617).ShouldBeLessThan(0.001);
+            Math.Abs(targetPoint.Y - 3281841.8601).ShouldBeLessThan(0.001);
+        }
+        [Fact]
+        public void TransformaThrowExceptionTest()
+        {
+            var standardPoints = new Dictionary<Point, Point>()
+            {
+                { new Point(653068.71271461656, 3281704.0923241354), new Point(651796.2895339021, 3281825.0074003497) }
+            };
+            try
+            {
+                new TransformationParam(standardPoints);
+            }
+            catch (ArgumentException e)
+            {
+                e.ShouldNotBeNull();
+            }
+        }
+    }
+}
---
+++ XSX.Util/XSX/Util/PointTransforma.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace XSX.Util
+{
+    public class PointTransforma
+    {
+        public TransformationParam Transformation { get; }
+        private Vector<double> Delta { get; }
+        private Matrix<double> ScaleAndRoate { get; }
+        private VectorBuilder<double> V { get; }
+        public PointTransforma(TransformationParam transformation)
+        {
+            Transformation = transformation;
+            V = Vector<double>.Build;
+            Delta = V.Dense(new double[] { Transformation.DeltaX, Transformation.DeltaY });
+            this.ScaleAndRoate = Transformation.Scale * Matrix<double>.Build.Dense(2, 2, new double[]
+            {
+                Math.Cos(Transformation.Theta),
+                Math.Sin(Transformation.Theta),
+                -Math.Sin(Transformation.Theta),
+                Math.Cos(Transformation.Theta),
+            });
+        }
+
+        public Point Transforma(Point p)
+        {
+            var target = Delta + ScaleAndRoate * V.Dense(new double[] { p.X, p.Y });
+            return new Point() { X = target[0], Y = target[1] };
+        }
+    }
+    public class TransformationParam
+    {
+        /// <summary>
+        /// x偏移量
+        /// </summary>
+        public double DeltaX { get; private set; }
+        /// <summary>
+        /// y偏移量
+        /// </summary>
+        public double DeltaY { get; private set; }
+        /// <summary>
+        /// 缩放比例
+        /// </summary>
+        public double Scale { get; private set; }
+        /// <summary>
+        /// 旋转角
+        /// </summary>
+        public double Theta { get; private set; }
+        public TransformationParam(double deltaX, double deltaY, double scale, double theta)
+        {
+            this.DeltaX = deltaX;
+            this.DeltaY = deltaY;
+            this.Scale = scale;
+            this.Theta = theta;
+        }
+        /// <summary>
+        /// 计算转换参数
+        /// </summary>
+        /// <param name="standardPoints">标准点<![CDATA[<source,targe>]]></param>
+        /// <returns></returns>
+        public TransformationParam(Dictionary<Point, Point> standardPoints)
+        {
+            if (standardPoints.Count<2)
+            {
+                throw new ArgumentException("标准点数量不能小于2对。");
+            }
+            var M = Matrix<double>.Build;
+            var L = M.Dense(1, 2 * standardPoints.Count,
+                standardPoints.SelectMany(d => new double[] { d.Value.X - d.Key.X, d.Value.Y - d.Key.Y }).ToArray()).Transpose();
+            var B = M.Dense(4, 2 * standardPoints.Count, standardPoints.SelectMany(d => new[] { 1, 0, d.Key.X, -d.Key.Y, 0, 1, d.Key.Y, d.Key.X }).ToArray()).Transpose();
+            var P = M.DenseIdentity(standardPoints.Count*2);
+            var X = (B.Transpose() * P * B).Inverse() * B.Transpose() * P * L;
+            this.DeltaX = X[0, 0];
+            this.DeltaY = X[1, 0];
+            var a = X[2, 0];
+            var b = X[3, 0];
+            this.Scale = Math.Sqrt(Math.Pow(a + 1, 2) + Math.Pow(b, 2));
+            this.Theta = Math.Atan(b / (a + 1));
+        }
+    }
+    public class Point
+    {
+        public Point()
+        {
+
+        }
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
