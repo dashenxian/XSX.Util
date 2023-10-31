05f30d337cb957843fef1817217eefd723cc2d26
xiaoshenxian <125880321@qq.com>
2023-10-31 17:50:07 +0800

    增加7参数坐标转换

--- XSX.Util.Extension.Tests/XSX/Util/PointTransformaTest.cs
+++ XSX.Util.Extension.Tests/XSX/Util/PointTransformaTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Shouldly;
 using XSX.Util;
@@ -10,7 +11,7 @@ namespace Tests.XSX.Util
     public class PointTransformaTest
     {
         [Fact]
-        public void TransformaTest()
+        public void TransformaFourTest()
         {
             var standardPoints = new Dictionary<Point, Point>()
             {
@@ -20,14 +21,14 @@ namespace Tests.XSX.Util
                 { new Point(653178.5482, 3281675.9410), new Point(651904.7691, 3281857.9999) }
             };
             
-            var transformaParam = new TransformationParam(standardPoints);
-            var pointTransforma = new PointTransforma(transformaParam);
-            var targetPoint = pointTransforma.Transforma(new Point(653122.0487, 3281691.3951));
+            var transformaParam = new TransformationParamFour(standardPoints);
+            var pointTransforma = new PointTransformFour(transformaParam);
+            var targetPoint = pointTransforma.Transform(new Point(653122.0487, 3281691.3951));
             Math.Abs(targetPoint.X - 651848.4617).ShouldBeLessThan(0.001);
             Math.Abs(targetPoint.Y - 3281841.8601).ShouldBeLessThan(0.001);
         }
         [Fact]
-        public void TransformaThrowExceptionTest()
+        public void TransformaFourThrowExceptionTest()
         {
             var standardPoints = new Dictionary<Point, Point>()
             {
@@ -35,7 +36,73 @@ namespace Tests.XSX.Util
             };
             try
             {
-                new TransformationParam(standardPoints);
+                new TransformationParamFour(standardPoints);
+            }
+            catch (ArgumentException e)
+            {
+                e.ShouldNotBeNull();
+            }
+        }
+        [Fact]
+        public void TransformaSevenTest()
+        {
+            var standardPoints = new Dictionary<Point3D, Point3D>()
+            {
+                {new Point3D(-2066241.5001,5360801.8835,2761896.3022),new Point3D(-2066134.4869,5360847.0595,2761895.5970)},
+                {new Point3D(-1983936.0407,5430615.7282,2685375.7214),new Point3D(-1983828.7084,5430658.9827,2685374.6681)},
+                {new Point3D(-1887112.7302,5468749.1944,2677688.9806),new Point3D(-1887005.1714,5468790.6487,2677687.2680)},
+                {new Point3D(-1808505.4212,5512502.2716,2642356.5720),new Point3D(-1808397.7260,5512542.0921,2642354.4550)},
+                {new Point3D(-1847017.0670,5573542.7934,2483802.9904),new Point3D(-1846909.0036,5573582.6511,2483801.6147)},
+            };
+
+            var transformaParam = new TransformationParamSeven(standardPoints);
+            var pointTransforma = new PointTransformSeven(transformaParam);
+            var sourcePoint = new Point3D(-2066241.5001, 5360801.8835, 2761896.3022);
+            var expectTargetPoint = new Point3D(-2066134.4869, 5360847.0595, 2761895.5970);
+            var targetPoint = pointTransforma.Transform(sourcePoint);
+            Math.Abs(expectTargetPoint.X - targetPoint.X).ShouldBeLessThan(0.0349);
+            Math.Abs(expectTargetPoint.Y - targetPoint.Y).ShouldBeLessThan(0.0349);
+            Math.Abs(expectTargetPoint.Z - targetPoint.Z).ShouldBeLessThan(0.0349);
+            Math.Abs(transformaParam.Sigma- 0.0349).ShouldBeLessThan(0.001);
+        }
+        [Fact]
+        public void TransformaSevenSigmaTest()
+        {
+            var standardPoints = new Dictionary<Point3D, Point3D>()
+            {
+                {new Point3D(-1964642.836,4484908.586,4075486.898),new Point3D(-1964734.964,4484768.547,4075386.77 )},
+                {new Point3D(-1967082.716,4490541.646,4068048.151),new Point3D(-1967174.802,4490401.508,4067948.166)},
+                {new Point3D(-1958106.37 ,4482074.179,4082054.321),new Point3D(-1958198.61 ,4481934.193,4081954.089)},
+                {new Point3D(-1958396.995,4485396.445,4077966.297),new Point3D(-1958489.229,4485256.399,4077866.134)},
+                {new Point3D(-1953364.459,4481502.655,4084942.265),new Point3D(-1953456.789,4481362.679,4084841.963)},
+                {new Point3D(-1957928.755,4492765.305,4070011.563),new Point3D(-1958020.995,4492625.151,4069911.537)},
+            };
+
+            var transformaParam = new TransformationParamSeven(standardPoints);
+            var sourcePoints=new List<Point3D>()
+            {
+                new Point3D(-1.9545e+06,4.4903e+06,4.0742e+06),
+                new Point3D(-1.9520e+06,4.4853e+06,4.0811e+06),
+                new Point3D(-1.9569e+06,4.4974e+06,4.0652e+06),
+                new Point3D(-1.9519e+06,4.4949e+06,4.0704e+06),
+                new Point3D(-1.9535e+06,4.5010e+06,4.0629e+06),
+            };
+
+            var pointTransforma = new PointTransformSeven(transformaParam);
+            var targetPoint = pointTransforma.Transform(sourcePoints).ToList();
+            Math.Abs(transformaParam.Sigma - 0.007287575001997314).ShouldBeLessThan(0.001);
+        }
+        [Fact]
+        public void TransformaSevenThrowExceptionTest()
+        {
+            var standardPoints = new Dictionary<Point3D, Point3D>()
+            {
+                { new Point3D(653068.71271461656, 3281704.0923241354,0), new Point3D(651796.2895339021, 3281825.0074003497,0) },
+                { new Point3D(653068.71271461656, 3281704.0923241354,0), new Point3D(651796.2895339021, 3281825.0074003497,0) }
+            };
+            try
+            {
+                new TransformationParamSeven(standardPoints);
             }
             catch (ArgumentException e)
             {
--- XSX.Util/XSX/Util/PointTransforma.cs
+++ XSX.Util/XSX/Util/PointTransforma.cs
@@ -7,13 +7,29 @@ using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace XSX.Util
 {
-    public class PointTransforma
+    /// <summary>
+    /// 四参数转换
+    /// </summary>
+    public class PointTransformFour
     {
-        public TransformationParam Transformation { get; }
+        /// <summary>
+        /// 转换参数
+        /// </summary>
+        public TransformationParamFour Transformation { get; }
+        /// <summary>
+        /// 三个坐标平移量
+        /// </summary>
         private Vector<double> Delta { get; }
+        /// <summary>
+        /// 旋转和缩放量
+        /// </summary>
         private Matrix<double> ScaleAndRoate { get; }
         private VectorBuilder<double> V { get; }
-        public PointTransforma(TransformationParam transformation)
+        /// <summary>
+        /// 初始化四参数转换
+        /// </summary>
+        /// <param name="transformation">转换参数</param>
+        public PointTransformFour(TransformationParamFour transformation)
         {
             Transformation = transformation;
             V = Vector<double>.Build;
@@ -26,14 +42,30 @@ namespace XSX.Util
                 Math.Cos(Transformation.Theta),
             });
         }
-
-        public Point Transforma(Point p)
+        /// <summary>
+        /// 坐标转换
+        /// </summary>
+        /// <param name="p">要转换的点</param>
+        /// <returns></returns>
+        public Point Transform(Point p)
         {
             var target = Delta + ScaleAndRoate * V.Dense(new double[] { p.X, p.Y });
             return new Point() { X = target[0], Y = target[1] };
         }
+        /// <summary>
+        /// 坐标转换
+        /// </summary>
+        /// <param name="points">要转换的点列表</param>
+        /// <returns></returns>
+        public IEnumerable<Point> Transform(IEnumerable<Point> points)
+        {
+            return points.Select(d => Transform(d));
+        }
     }
-    public class TransformationParam
+    /// <summary>
+    /// 四参数转换模型
+    /// </summary>
+    public class TransformationParamFour
     {
         /// <summary>
         /// x偏移量
@@ -51,7 +83,7 @@ namespace XSX.Util
         /// 旋转角
         /// </summary>
         public double Theta { get; private set; }
-        public TransformationParam(double deltaX, double deltaY, double scale, double theta)
+        public TransformationParamFour(double deltaX, double deltaY, double scale, double theta)
         {
             this.DeltaX = deltaX;
             this.DeltaY = deltaY;
@@ -63,9 +95,9 @@ namespace XSX.Util
         /// </summary>
         /// <param name="standardPoints">标准点<![CDATA[<source,targe>]]></param>
         /// <returns></returns>
-        public TransformationParam(Dictionary<Point, Point> standardPoints)
+        public TransformationParamFour(Dictionary<Point, Point> standardPoints)
         {
-            if (standardPoints.Count<2)
+            if (standardPoints.Count < 2)
             {
                 throw new ArgumentException("标准点数量不能小于2对。");
             }
@@ -73,7 +105,7 @@ namespace XSX.Util
             var L = M.Dense(1, 2 * standardPoints.Count,
                 standardPoints.SelectMany(d => new double[] { d.Value.X - d.Key.X, d.Value.Y - d.Key.Y }).ToArray()).Transpose();
             var B = M.Dense(4, 2 * standardPoints.Count, standardPoints.SelectMany(d => new[] { 1, 0, d.Key.X, -d.Key.Y, 0, 1, d.Key.Y, d.Key.X }).ToArray()).Transpose();
-            var P = M.DenseIdentity(standardPoints.Count*2);
+            var P = M.DenseIdentity(standardPoints.Count * 2);
             var X = (B.Transpose() * P * B).Inverse() * B.Transpose() * P * L;
             this.DeltaX = X[0, 0];
             this.DeltaY = X[1, 0];
@@ -96,5 +128,152 @@ namespace XSX.Util
         }
         public double X { get; set; }
         public double Y { get; set; }
+    }
+    /// <summary>
+    /// 布尔莎七参数坐标转换
+    /// </summary>
+    public class PointTransformSeven
+    {
+        /// <summary>
+        /// 转换参数
+        /// </summary>
+        public TransformationParamSeven Transformation { get; }
+        /// <summary>
+        /// 转换参数
+        /// </summary>
+        private Matrix<double> ScaleAndRoate { get; }
+        /// <summary>
+        /// 矩阵初始化器
+        /// </summary>
+        private MatrixBuilder<double> M { get; }
+        /// <summary>
+        /// 初始化布尔莎七参数坐标转换
+        /// </summary>
+        /// <param name="transformation">转换参数</param>
+        public PointTransformSeven(TransformationParamSeven transformation)
+        {
+            Transformation = transformation;
+            M = Matrix<double>.Build;
+            this.ScaleAndRoate = M.Dense(1, 7, new double[]
+            {
+                Transformation.DeltaX,
+                Transformation.DeltaY,
+                Transformation.DeltaZ,
+                Transformation.ThetaX,
+                Transformation.ThetaY,
+                Transformation.ThetaZ,
+                Transformation.Scale
+            }).Transpose();
+        }
+        /// <summary>
+        /// 坐标转换
+        /// </summary>
+        /// <param name="p">要转换的点</param>
+        /// <returns></returns>
+        public Point3D Transform(Point3D p)
+        {
+            var B = M.Dense(7, 3, new double[] {
+                    1,0,0,0,-p.Z,p.Y,p.X,
+                    0,1,0,p.Z, 0,-p.X, p.Y,
+                    0,0,1,-p.Y, p.X,0, p.Z
+                        }).Transpose();
+            var target = B.Multiply(ScaleAndRoate);
+            return new Point3D() { X = target[0, 0] + p.X, Y = target[1, 0] + p.Y, Z = target[2, 0] + p.Z };
+        }
+        /// <summary>
+        /// 坐标转换
+        /// </summary>
+        /// <param name="points">要转换的点列表</param>
+        /// <returns></returns>
+        public IEnumerable<Point3D> Transform(IEnumerable<Point3D> points)
+        {
+            return points.Select(d => Transform(d));
+        }
+    }
+    /// <summary>
+    /// 布尔莎七参数模型
+    /// </summary>
+    public class TransformationParamSeven
+    {
+        /// <summary>
+        /// x偏移量
+        /// </summary>
+        public double DeltaX { get; private set; }
+        /// <summary>
+        /// y偏移量
+        /// </summary>
+        public double DeltaY { get; private set; }
+        /// <summary>
+        /// Z偏移量
+        /// </summary>
+        public double DeltaZ { get; private set; }
+        /// <summary>
+        /// 缩放比例
+        /// </summary>
+        public double Scale { get; private set; }
+        /// <summary>
+        /// 旋转角X
+        /// </summary>
+        public double ThetaX { get; private set; }
+        /// <summary>
+        /// 旋转角Y
+        /// </summary>
+        public double ThetaY { get; private set; }
+        /// <summary>
+        /// 旋转角Z
+        /// </summary>
+        public double ThetaZ { get; private set; }
+        /// <summary>
+        /// 精度评定，中误差
+        /// </summary>
+        public double Sigma { get; set; }
+        public TransformationParamSeven(Dictionary<Point3D, Point3D> standardPoints)
+        {
+            if (standardPoints.Count < 3)
+            {
+                throw new ArgumentException("标准点数量不能小于3对。");
+            }
+            var M = Matrix<double>.Build;
+            var L = M.Dense(1, 3 * standardPoints.Count,
+                standardPoints.SelectMany(d => new double[] { d.Value.X - d.Key.X, d.Value.Y - d.Key.Y, d.Value.Z - d.Key.Z }).ToArray()).Transpose();
+            var B = M.Dense(7, 3 * standardPoints.Count, standardPoints.SelectMany(d => new[]
+            {
+                1, 0, 0, 0, - d.Key.Z, d.Key.Y, d.Key.X,
+                0, 1, 0, d.Key.Z, 0, -d.Key.X, d.Key.Y,
+                0, 0, 1, -d.Key.Y, d.Key.X, 0, d.Key.Z
+            }).ToArray()).Transpose();
+            var X = (B.Transpose() * B).Inverse() * (B.Transpose() * L);
+            this.DeltaX = X[0, 0];
+            this.DeltaY = X[1, 0];
+            this.DeltaZ = X[2, 0];
+            var a1 = X[3, 0];
+            var a2 = X[4, 0];
+            var a3 = X[5, 0];
+            var a4 = X[6, 0];
+            this.ThetaX = a1;
+            this.ThetaY = a2;
+            this.ThetaZ = a3;
+            this.Scale = a4;
+            var V = B * X - L;
+            var P = M.DenseIdentity(standardPoints.Count * 3);
+            Sigma = Math.Sqrt((V.Transpose() * P * V)[0, 0] / (standardPoints.Count * 3 - 7));
+        }
+    }
+
+    public class Point3D
+    {
+        public Point3D()
+        {
+
+        }
+        public Point3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            Z = z;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
     }
 }
